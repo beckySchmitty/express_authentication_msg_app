d7777a1798353992d400c435b0e19dfae3601af2
Becky Schmitthenner <becky.schmitthenner@gmail.com>
2021-01-30 15:09:35 -0500

    register/authenticate/updateLoginTS added to User class

--- models/user.js
+++ models/user.js
@@ -1,28 +1,55 @@
 /** User class for message.ly */
 
+const db = require("../db");
+const bcrypt = require("bcrypt");
+const ExpressError = require("../expressError");
 
+const { BCRYPT_WORK_FACTOR } = require("../config");
 
 /** User of the site. */
-
 class User {
 
   /** register new user -- returns
    *    {username, password, first_name, last_name, phone}
    */
+  static async register({username, password, first_name, last_name, phone}) { 
+    let hashedPwd = await bcrypt.hash(password, BCRYPT_WORK_FACTOR)
+    const results = await db.query(`INSERT INTO users
+    (username, password, first_name, last_name, phone, join_at, last_login_at)
+    VALUES ($1, $2, $3, $4, $5, current_timestamp, current_timestamp) 
+    RETURNING username, password, first_name, last_name, phone`
+    , [username, hashedPwd, first_name, last_name, phone]);
 
-  static async register({username, password, first_name, last_name, phone}) { }
+    return results.rows[0];
+  }
 
   /** Authenticate: is this username/password valid? Returns boolean. */
+  static async authenticate(username, password) { 
+    const results = await db.query(`SELECT * FROM user WHERE username=$1`, [username]);
+    const user = results.rows[0];
 
-  static async authenticate(username, password) { }
+    if (!user) {
+      throw new ExpressError(`User with username ${username} not found`, 401);
+    }
+    // returns true if passwords match
+    return user && await bcrypt.compare(password, user.password);
+  }
 
   /** Update last_login_at for user */
+  static async updateLoginTimestamp(username) {
+    const results = await db.query(`UPDATE users
+    SET last_login = current_timestamp 
+    WHERE username=$1
+    RETURNING username`, [username]);
 
-  static async updateLoginTimestamp(username) { }
+    if (!results.rows[0]) {
+      throw new ExpressError(`User with username ${username} not found`, 401)
+    }
+
+   }
 
   /** All: basic info on all users:
    * [{username, first_name, last_name, phone}, ...] */
-
   static async all() { }
 
   /** Get: get user by username
@@ -33,7 +60,6 @@ class User {
    *          phone,
    *          join_at,
    *          last_login_at } */
-
   static async get(username) { }
 
   /** Return messages from this user.
@@ -43,7 +69,6 @@ class User {
    * where to_user is
    *   {username, first_name, last_name, phone}
    */
-
   static async messagesFrom(username) { }
 
   /** Return messages to this user.
@@ -53,7 +78,6 @@ class User {
    * where from_user is
    *   {id, first_name, last_name, phone}
    */
-
   static async messagesTo(username) { }
 }
 
